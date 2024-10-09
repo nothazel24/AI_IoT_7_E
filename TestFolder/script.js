3b2cab147d89335514496bcda1d989ba21024344
NzddXD <muhammadnezad1212@gmail.com>
2024-10-09 09:42:54 +0700

    Bebas

--- TestFolder/script.js
+++
@@ -1,9 +0,0 @@
-let overlay = document.getElementById("overlay")
-// let btn = document.getElementById("btn")
-
-function openOverlay(){
-    overlay.style.height = 100 + "%"
-}
-function closeOverlay(){
-    overlay.style.height = 0 + "%"
-}
