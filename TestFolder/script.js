e3af543099290d40c93dcd0589c8d03dc1842ebe
NzddXD <muhammadnezad1212@gmail.com>
2024-10-09 08:53:16 +0700

    My test commits :3

---
+++ TestFolder/script.js
@@ -0,0 +1,9 @@
+let overlay = document.getElementById("overlay")
+// let btn = document.getElementById("btn")
+
+function openOverlay(){
+    overlay.style.height = 100 + "%"
+}
+function closeOverlay(){
+    overlay.style.height = 0 + "%"
+}
